daae08cb25ef6d013653830175bbbf243b69597d
Bohdan Pakhomov <shtormless@gmail.com>
2020-04-15 16:17:00 +0300

    app: working with postgres[fixed]

--- internal/app/store/store_test.go
+++ internal/app/store/store_test.go
@@ -12,7 +12,7 @@ var (
 func TestMain(m *testing.M) {
 	databaseURL = os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
-		databaseURL = "host=localhost dbname=restapi_test sslmode=disable"
+		databaseURL = "host=localhost user=postgres dbname=restapi_test sslmode=disable"
 	}
 
 	os.Exit(m.Run())
